ede6d2998040da208e9ca8826f893b985f8555c3
Ionut <ionut02444@gmail.com>
2023-08-31 21:02:13 +0300

    Practice session 11!

---
+++ src/session11/abstractionpractice/Playable.java
@@ -0,0 +1,31 @@
+package session11.abstractionpractice;
+
+public interface Playable {
+
+    void play();
+    void stop();
+}
+
+class Music implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Music is playing");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Music is stopped");
+    }
+}
+
+class Video implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Video is playing");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Video is stopped");
+    }
+}
